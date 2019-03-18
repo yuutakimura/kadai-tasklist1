012fdf18cb22bf08b96557cf73249d9191fcf89c
kimuyu.cocoa <kimu.cocoa@icloud.com>
2019-03-18 17:59:03 +0000

    add column

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -51,6 +51,6 @@ class MessagesController < ApplicationController
   end
 
   def message_params
-    params.require(:message).permit(:content, :title)
+    params.require(:message).permit(:content)
   end
 end
