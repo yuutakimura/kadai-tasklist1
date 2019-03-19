c0797aec2e562ac207fe5d9a93daddc48797e9ff
kimuyu.cocoa <kimu.cocoa@icloud.com>
2019-03-19 17:49:51 +0000

    CRUD messages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,57 +1,59 @@
 class TasksController < ApplicationController
+  
   def index
-      @tasks = Task.all
-  end
-
+    @tasks = Task.all
+  end 
+  
   def show
-      @task = Task.find(params[:id])
-  end
-
-  def new
-      @task = Task.new
-  end
-
+    @task = Task.find(params[:id])
+  end 
+  
+  def new 
+    @task = Task.new
+  end 
+  
   def create
     @task = Task.new(task_params)
-
+    
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
+      flash[:success] = '正常に登録されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      flash.now[:danger] = '登録されませんでした'
+      render:new
     end
   end
-
+  
   def edit
-       @task = Task.find(params[:id])
-  end
-
+    @task = Task.find(params[:id])
+  end 
+  
   def update
     @task = Task.find(params[:id])
-
+    
     if @task.update(task_params)
-      flash[:success] = 'Message は正常に更新されました'
+      flash[:success] = '更新できました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Message は更新されませんでした'
-      render :edit
+      flash.now[:danger] = '更新できませんでした'
+      render:edit
     end
-  end
-
+  end 
+  
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
+    
+    flash[:success] ='削除しました'
+    redirect_to tasks_url
+  end 
 
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to @task
-  end
   
   private
-
-  # Strong Parameter
-  def task_params
-    params.require(:task).permit(:content,:status)
-  end
+  #Strong Parameter
   
-end
+  def task_params
+    params.require(:task).permit(:content)
+  end 
+  
+end 
