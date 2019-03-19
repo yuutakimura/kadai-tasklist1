1bdadbefdd61039bb143916b16156144514ef061
kimuyu.cocoa <kimu.cocoa@icloud.com>
2019-03-19 17:44:45 +0000

    CRUD messages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,56 +1,57 @@
 class TasksController < ApplicationController
   def index
-    @task = Tasks.all
+      @tasks = Task.all
   end
 
   def show
-    @tasks = tasks.find(params[:id])
+      @task = Task.find(params[:id])
   end
 
   def new
-    @tasks = Tasks.new
+      @task = Task.new
   end
 
   def create
-    @tasks = tasks.new(message_params)
+    @task = Task.new(task_params)
 
-    if @tasks.save
-      flash[:success] = 'tasks が正常に投稿されました'
-      redirect_to @tasks
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
     else
-      flash.now[:danger] = 'tasks が投稿されませんでした'
+      flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
   end
 
   def edit
-    @tasks = Tasks.find(params[:id])
+       @task = Task.find(params[:id])
   end
 
   def update
-    @tasks = Tasks.find(params[:id])
+    @task = Task.find(params[:id])
 
-    if @tasks.update(message_params)
-      flash[:success] = 'tasks は正常に更新されました'
-      redirect_to @tasks
+    if @task.update(task_params)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @task
     else
-      flash.now[:danger] = 'tasks は更新されませんでした'
+      flash.now[:danger] = 'Message は更新されませんでした'
       render :edit
     end
   end
 
   def destroy
-     @tasks = Tasks.find(params[:id])
-    @tasks.destroy
+    @task = Task.find(params[:id])
+    @task.destroy
 
-    flash[:success] = 'tasks は正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to @task
   end
   
-   private
+  private
 
   # Strong Parameter
-  def message_params
-    params.require(:tasks).permit(:content)
+  def task_params
+    params.require(:task).permit(:content,:status)
   end
-end 
+  
+end
